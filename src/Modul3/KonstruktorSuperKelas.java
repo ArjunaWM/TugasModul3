5648aa3a7bb58e8d9c04df33a754ddd8f32d6e06
ARJUNA <ARJUNA@LAPTOP-0HAV1RQA>
2018-09-21 09:39:46 +0700

    Commit keseluruhan

---
+++ src/Modul3/KonstruktorSuperKelas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul3;
+
+/**
+ *
+ * @author ARJUNA
+ */
+public class KonstruktorSuperKelas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Employ programmer1 = new Employ("12345678", "Arjuna Wira mahardika", 9);
+        programmer1.info();
+    }    
+}
---
+++ src/Modul3/PertamaKedua.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul3;
+
+/**
+ *
+ * @author ARJUNA
+ */
+public class PertamaKedua {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          Kedua D2 = new Kedua();
+        D2.BacaSuper ();
+        D2.info ();
+       
+        Pertama S1 = new Pertama ();
+        S1.terprotek ();
+        S1.info ();
+    }
+    
+}
